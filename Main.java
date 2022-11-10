8881f010c241dda2b7257b8c198c2129b60ae560
Christopher Stidwill <chris.stidwill@gmail.com>
2022-11-10 08:53:37 +0000

    Created a population growth calculator.

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        double population = 5000;
+        double growthRate = 1.05;
+        int years = 10;
+        System.out.println("The population will be " + population * (growthRate * Math.pow(growthRate, years)));
     }
 }
